080da7476d78360adcce1f0d759d515e29e30ce5
DamonEccl <damon.eccles@hotmail.com>
2024-06-12 14:13:16 +1000

    Basic Jframe stuff

--- passwordManager/src/App.java
+++ passwordManager/src/App.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
+
 public class App {
+    public static JFrame frame = new JFrame();
+    public static JPanel panel = new JPanel();
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        frame.setSize(1000, 1000);
+        frame.setVisible(true);
     }
 }
